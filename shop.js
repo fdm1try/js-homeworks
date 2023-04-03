4f14e7996f22abf0d91933f31ce14785a8e9d6ae
fdm1try <94063329+fdm1try@users.noreply.github.com>
2023-04-03 13:37:23 +0300

    complete lesson 3

--- shop.js
+++ shop.js
@@ -1,141 +1,145 @@
-const goods = [
-    {
-        id: 0, 
-        name: 'Футболка мужская',
-        description: 'Мужская футболка белая', 
-        sizes: ['s', 'm', 'xl'],
-        price: 799,
-        available: true
-    },
-    {
-        id: 1, 
-        name: 'Брюки мужские',
-        description: 'Брюки мужские черные', 
-        sizes: ['xs', 'l', 'xxl'],
-        price: 1399,
-        available: true
-    },
-    {
-        id: 2, 
-        name: 'Пиджак женский',
-        description: 'Женский пиджак, белый', 
-        sizes: ['xs', 's', 'm'],
-        price: 10890,
-        available: true
-    },
-    {
-        id: 3, 
-        name: 'Сорочка женская',
-        description: 'Сорочка женская, фиолетовая', 
-        sizes: ['s', 'm', 'l'],
-        price: 299,
-        available: true
-    },
-    {
-        id: 4, 
-        name: 'Шорты',
-        description: 'Шорты унисекс, розовые', 
-        sizes: ['s', 'm', 'xl'],
-        price: 500,
-        available: true
-    },
-    {
-        id: 5, 
-        name: 'Кимоно женское',
-        description: 'Женское кимоно, разноцветное', 
-        sizes: ['l', 'xl', 'xxl', 'xxxl'],
-        price: 6900,
-        available: false
+class Good {
+    constructor(id, name, description, price, sizes=[], available=true){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.sizes = sizes;
+        this.available = !!available;
     }
-]
 
-let basket = [
-    {
-        good: goods[0],
-        amount: 1
-    },
-    {
-        good: goods[3],
-        amount: 2
+    setAvaliable(value){
+        this.available = !!value;
     }
-]
+}
 
-function addItemToBasket(good_id, amount=1){
-    let good = goods.find(good => good.id == good_id);
-    if (!good) {
-        console.error('Нет товара с указанным ID!');
+class GoodsList {
+    #goods = [];
+    filter = /.*/;
+    sortPrice = true;
+    sortDir = false;
+
+    get list(){
+        let list = this.#goods.filter(good => this.filter.test(good.name));
+        if (this.sortPrice){
+            list = list.sort((a, b) => this.sortDir ? a.price - b.price : b.price - a.price);
+        }
+        return list;
+    }
+
+    add(good){
+        if (good instanceof Good){
+            this.#goods.push(good);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    remove(good_id){
+        let item_index = this.#goods.findIndex(good => good.id === good_id);
+        if (item_index >= 0){
+            this.#goods.splice(item_index, 1);
+        }
+    }
+}
+
+class BasketGood extends Good {
+    amount = 0;
+
+    constructor(good, amount){
+        super(good.id, good.name, good.description, good.price, good.sizes, good.available);
+        this.amount = amount;
+    }
+}
+
+class Basket {
+    goods = []
+
+    get totalAmount(){
+        return this.goods.reduce((total, good) => total + good.amount, 0);
+    }
+
+    get totalSum(){
+        return this.goods.reduce((total, good) => total + good.amount * good.price, 0);
+    }
+
+    add(good, amount){
+        if (good instanceof Good){
+            let item = this.goods.find(item => item.id === good.id);
+            if (item)
+                item.amount +=amount;
+            else
+                this.goods.push(new BasketGood(good, amount));
+            return true;
+        }
         return false;
     }
-    if (!good.available){
-        console.error('Товар "' + good.name + '" не доступен для покупки!');
+
+    remove(good, amount){
+        if (good instanceof Good) {
+            let item_index = this.goods.findIndex(item => item.id === good.id);
+            if (item_index < 0)
+                return false;
+            let item = this.goods[item_index];
+            item.amount -= amount;
+            if (item.amount < 1)
+                this.goods.splice(item_index, 1);
+            return true;
+        }
         return false;
     }
-    let item = basket.find(item => item.good.id == good.id)
-    if (item){
-        item.amount += amount;
-    }
-    else {
-        basket.push({ good, amount });
-    }
-    return true;
-}
 
-function removeItemFromBasket(good_id){
-    let item_index = basket.findIndex(item => item.good.id == good_id);
-    if (item_index >= 0){
-        return basket.splice(item_index, 1);
+    clear(){
+        this.goods = [];
+    }
+
+    removeUnavailiable(){
+        this.goods = this.goods.filter(good => good.available);
     }
 }
 
-function clearBasket(){
-    basket = [];
-}
-
-function getTotalAmountAndSum(){
-    let result = {
-        totalAmount: 0,
-        totalSum: 0
-    };
-    for (let item of basket){
-        result.totalAmount += item.amount;
-        result.totalSum += item.amount * item.good.price;
+function printBasket(basket, title){
+    console.log(title);
+    let maxChars = basket.goods.reduce((max, good) => max > good.name.length ? max : good.name.length, 0);
+    let splitterCount = maxChars + 3;
+    for (let good of basket.goods){
+        console.log(good.name + Array(splitterCount + 1 - good.name.length).join('-') + good.amount + ' шт.\t' + (good.amount * good.price));
     }
-    return result;
+    console.log('Итого' + Array(splitterCount - 4).join('-') + basket.totalAmount + ' шт.\t' + basket.totalSum);
+
 }
 
-let total = getTotalAmountAndSum();
-console.log('Изначально в корзине было ' + total.totalAmount + ' товар(а/ов) на сумму ' + total.totalSum);
-clearBasket();
-console.log('Корзина очищена...')
-if (addItemToBasket(1, 4)){
-    console.log('Добавлен товар "' + goods[1].name + '" (4 шт.)')
-} else {
-    console.log('Не удалось добавить товар...')
+let goods = new GoodsList();
+goods.add(new Good(0, 'Футболка мужская', 'Мужская футболка белая', 799, ['s', 'm', 'xl'], true));
+goods.add(new Good(1, 'Брюки мужские', 'Брюки мужские черные', 1399, ['xs', 'l', 'xxl'], true));
+goods.add(new Good(2, 'Пиджак женский', 'Женский пиджак, белый', 10890, ['xs', 's', 'm'], true));
+goods.add(new Good(3, 'Сорочка женская', 'Сорочка женская, фиолетовая', 299, ['s', 'm', 'l'], true));
+goods.add(new Good(4, 'Шорты унисекс', 'Шорты, розовые', 500, ['s', 'm', 'xl'], true));
+goods.add(new Good(5, 'Кимоно женское', 'Женское кимоно, разноцветное', 6900, ['l', 'xl', 'xxl', 'xxxl'], false));
+
+let basket = new Basket();
+goods.filter = /жен|унисекс/i;
+for (let good of goods.list){
+    basket.add(good, 1);
 }
-if (addItemToBasket(2, 1)){
-    console.log('Добавлен товар "' + goods[2].name + '" (1 шт.)')
-} else {
-    console.log('Не удалось добавить товар...')
+console.log('Всего женских товаров в магазине ' + basket.totalAmount + ' шт. Они стоят: ' + basket.totalSum);
+basket.clear();
+goods.filter = /муж|унисекс/i;
+for (let good of goods.list){
+    basket.add(good, 1);
 }
-if (addItemToBasket(2, 1)){
-    console.log('Добавлен товар "' + goods[2].name + '" (1 шт.)')
-} else {
-    console.log('Не удалось добавить товар...')
+console.log('Всего мужских товаров в магазине ' + basket.totalAmount + ' шт. Они стоят: ' + basket.totalSum);
+basket.clear();
+
+goods.remove(4);
+goods.sortDir = true;
+goods.filter = /.*/;
+for (let good of goods.list){
+    basket.add(good, 3);
 }
-if (addItemToBasket(1, 10)){
-    console.log('Добавлен товар "' + goods[1].name + '" (10 шт.)')
-} else {
-    console.log('Не удалось добавить товар...')
-}
-if (removeItemFromBasket(1)){
-    console.log('Удален товар "' + goods[1].name + '"');
-} else {
-    console.log('Не удалось убрать товар из корзины...')
-}
-if (addItemToBasket(5, 1)) {
-    console.log('Добавлен товар "' + goods[5].name + '" (1 шт.)')
-} else {
-    console.log('Не удалось добавить товар...')
-}
-total = getTotalAmountAndSum();
-console.log('Теперь в корзине ' + total.totalAmount + ' товар(а/ов) на сумму ' + total.totalSum);
+basket.removeUnavailiable();
+basket.remove(basket.goods[basket.goods.length - 1], basket.goods[basket.goods.length - 1].amount);
+basket.goods[basket.goods.length - 1]
+basket.remove(basket.goods[1], basket.goods[0].amount - 1);
+printBasket(basket, '\nОставшиеся товары:');
